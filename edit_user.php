432d8db36a9e3d4bbdb3503157185a06caaf288b
gmentemas <160733926+gmentemas@users.noreply.github.com>
2024-04-23 02:18:27 +0800

    Add files via upload

---
+++ edit_user.php
@@ -0,0 +1,26 @@
+<?php
+include "../koneksi.php";
+$id=$_POST['id'];
+$nip=$_POST['nip'];
+$emial=$_POST['email'];
+$nama=$_POST['nama'];
+$prodi=$_POST['prodi'];
+$jabatan1=$_POST['jabatan'];
+if ($jabatan1==null) {
+	mysqli_query($koneksi,"UPDATE user SET nama='$nama', nip='$nip', email='$email', jenis='Dosen', prodi='$prodi' WHERE nip='$id'");
+	header("location:data.php?alert=berhasil");
+}else{
+	$jabat=mysqli_query($koneksi,"SELECT * FROM user WHERE jenis='Kaprodi' AND prodi='$prodi'");
+	$cek=mysqli_num_rows($jabat);
+
+	if($cek>0){
+		echo "<script>alert('Kaprodi Sudah diinput, silakan ubah kaprodi sebelumnya menjadi dosen pengajar'); history.go(-1);</script>";
+	}else{
+
+	mysqli_query($koneksi,"UPDATE user SET nama='$nama', nip='$nip', email='$email', jenis='$jabatan1', prodi='$prodi' WHERE nip='$id'");
+	header("location:data.php?alert=berhasil");
+	}
+}
+
+
+?>
